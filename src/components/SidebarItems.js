e9c6aa0706d6d3c43c32bd7b1bf41b29ef655549
Zhanyao93 <zhanyao1993@gmail.com>
2022-05-25 02:07:14 +0800

    fixed more css for both vert and hori sidebar

--- src/components/SidebarItems.js
+++ src/components/SidebarItems.js
@@ -12,7 +12,7 @@ function SidebarItem() {
         return(
             <span onClick={()=> handleCurCol(subTitle)}>
                 <i className="bi-pin-fill" />
-                <div className="sub-tile">{subTitle}</div>
+                <div className="sub-title">{subTitle}</div>
                 <i className="bi-pencil-square sub-btn" onClick={(e)=>{editColName(e,subTitle)}} />
                 <i className="bi-x-square sub-btn" onClick={(e)=>{delCol(e,subTitle)}}/>
             </span>
@@ -80,7 +80,9 @@ function SidebarItem() {
                 <i className="bi-chevron-down toggle-btn"/>
             </div>
             <div className="sidebar-content">
+                <div className="sub-titles-con">
                 {allSub()}
+                </div>
                 <div className="new-col-btn" onClick={addNewCol}>
                 New Collection
                 </div>
